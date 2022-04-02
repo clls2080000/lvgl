e3cd96a1d651fb353a53665b935ab3f7c65a905c
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-04-02 15:03:51 +0200

    transform handle edges

--- src/draw/sw/lv_draw_sw_img.c
+++ src/draw/sw/lv_draw_sw_img.c
@@ -193,40 +193,44 @@ void tranform_cb(const lv_area_t * dest_area, const void * src_buf, lv_coord_t s
                 int32_t ys_int = ys_ups >> 8;
 
                 /*Fully out of the image*/
-                if(xs_int < -1 || xs_int > src_w || ys_int < -1 || ys_int > src_h) {
+                if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
                     abuf[x] = 0x00;
                     ys_ups += ys_step;
                     xs_ups += xs_step;
                     continue;
                 }
 
-                if(xs_int > 0 && xs_int < src_w - 1 && ys_int > 0 && ys_int < src_h - 1) {
-                    /*Get the direction the hor and ver neighbor
-                     *`fract` will be in range of 0x00..0xFF and `next` (+/-1) indicates the direction*/
-                    int32_t xs_fract = xs_ups & 0xFF;
-                    int32_t ys_fract = ys_ups & 0xFF;
-                    int32_t x_next;
-                    int32_t y_next;
-                    if(xs_fract < 0x80) {
-                        x_next = -1;
-                        xs_fract = (0x7F - xs_fract) * 2;
-                    }
-                    else {
-                        x_next = 1;
-                        xs_fract = (xs_fract - 0x80) * 2;
-                    }
-                    if(ys_fract < 0x80) {
-                        y_next = -1;
-                        ys_fract = (0x7F - ys_fract) * 2;
-                    }
-                    else {
-                        y_next = 1;
-                        ys_fract = (ys_fract - 0x80) * 2;
-                    }
+                /*Get the direction the hor and ver neighbor
+                 *`fract` will be in range of 0x00..0xFF and `next` (+/-1) indicates the direction*/
+                int32_t xs_fract = xs_ups & 0xFF;
+                int32_t ys_fract = ys_ups & 0xFF;
+                int32_t x_next;
+                int32_t y_next;
+                if(xs_fract < 0x80) {
+                    x_next = -1;
+                    xs_fract = (0x7F - xs_fract) * 2;
+                }
+                else {
+                    x_next = 1;
+                    xs_fract = (xs_fract - 0x80) * 2;
+                }
+                if(ys_fract < 0x80) {
+                    y_next = -1;
+                    ys_fract = (0x7F - ys_fract) * 2;
+                }
+                else {
+                    y_next = 1;
+                    ys_fract = (ys_fract - 0x80) * 2;
+                }
 
-                    const uint8_t * src_tmp;
-                    src_tmp = trans_dsc.cfg.src;
-                    src_tmp += (ys_int * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE) + xs_int * LV_IMG_PX_SIZE_ALPHA_BYTE;
+                const uint8_t * src_tmp;
+                src_tmp = trans_dsc.cfg.src;
+                src_tmp += (ys_int * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE) + xs_int * LV_IMG_PX_SIZE_ALPHA_BYTE;
+
+                if(xs_int + x_next >= 0 &&
+                   xs_int + x_next <= src_w - 1 &&
+                   ys_int + y_next >= 0 &&
+                   ys_int + y_next <= src_h - 1) {
 
                     const uint8_t * px_base = NULL;
                     const uint8_t * px_hor = NULL;
@@ -239,8 +243,8 @@ void tranform_cb(const lv_area_t * dest_area, const void * src_buf, lv_coord_t s
                     lv_opa_t a_ver = px_ver[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
                     lv_opa_t a_hor = px_hor[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
 
-                    if(a_ver != a_base) a_ver = ((a_ver * ys_fract) + (a_base * (0xFF - ys_fract))) >> 8;
-                    if(a_hor != a_base) a_hor = ((a_hor * xs_fract) + (a_base * (0xFF - xs_fract))) >> 8;
+                    if(a_ver != a_base) a_ver = ((a_ver * ys_fract) + (a_base * (0x100 - ys_fract))) >> 8;
+                    if(a_hor != a_base) a_hor = ((a_hor * xs_fract) + (a_base * (0x100 - xs_fract))) >> 8;
                     abuf[x] = (a_ver + a_hor) >> 1;
 
                     if(abuf[x]) {
@@ -256,7 +260,18 @@ void tranform_cb(const lv_area_t * dest_area, const void * src_buf, lv_coord_t s
                     }
                 }
                 else {
-                    abuf[x] = 0x0;
+                    cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
+                    lv_opa_t a = src_tmp[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+
+                    if((xs_int == 0 && x_next < 0) || (xs_int == src_w - 1 && x_next > 0))  {
+                        abuf[x] = (a * (0xFF - xs_fract)) >> 8;
+                    }
+                    else if((ys_int == 0 && y_next < 0) || (ys_int == src_h - 1 && y_next > 0))  {
+                        abuf[x] = (a * (0xFF - ys_fract)) >> 8;
+                    }
+                    else {
+                        abuf[x] = 0x00;
+                    }
                 }
 
                 xs_ups += xs_step;
