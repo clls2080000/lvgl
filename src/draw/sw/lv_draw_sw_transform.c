44adc34be646ca9f31ade9e0d34f326633248d98
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-04-04 16:50:52 +0200

    support more color formats

--- src/draw/sw/lv_draw_sw_transform.c
+++ src/draw/sw/lv_draw_sw_transform.c
@@ -49,6 +49,19 @@ typedef struct {
  *  STATIC PROTOTYPES
  **********************/
 
+static void argb_no_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                       int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                       int32_t x_end, lv_color_t * cbuf, uint8_t * abuf);
+
+static void rgb_no_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                      int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                      int32_t x_end, lv_color_t * cbuf, uint8_t * abuf);
+
+static void argb_and_rgb_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                            int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                            int32_t x_end, lv_color_t * cbuf, uint8_t * abuf, bool has_alpha);
+
+
 static void transform_point(const lv_img_transform_dsc_t * dsc, int32_t x, int32_t y, int32_t * xs, int32_t * ys);
 static void _lv_img_buf_transform_init(lv_img_transform_dsc_t * dsc);
 
@@ -96,121 +109,16 @@ void lv_draw_sw_transform(lv_draw_ctx_t * draw_ctx, const lv_area_t * dest_area,
         int32_t xs_ups = xs1_ups + xs_step / 2;     /*Init. + go the center of the pixel*/
         int32_t ys_ups = ys1_ups + ys_step / 2;
 
-        lv_coord_t x;
         if(draw_dsc->antialias == 0) {
-            for(x = 0; x < dest_w; x++) {
-                int32_t xs_int = xs_ups >> 8;
-                int32_t ys_int = ys_ups >> 8;
-                if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
-                    abuf[x] = 0;
-                }
-                else {
-                    const uint8_t * src_tmp;
-                    src_tmp = trans_dsc.cfg.src;
-                    src_tmp += (ys_int * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE) + xs_int * LV_IMG_PX_SIZE_ALPHA_BYTE;
-
-#if LV_COLOR_DEPTH == 16
-                    cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
-#elif LV_COLOR_DEPTH == 32
-                    cbuf[x].full = *((uint32_t *)src_tmp);
-#endif
-                    //                    if(has_alpha)
-                    abuf[x] = src_tmp[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
-                }
-                xs_ups += xs_step;
-                ys_ups += ys_step;
+            if(cf == LV_IMG_CF_RGBA) {
+                argb_no_aa(src_buf, src_w, src_h, src_stride, xs_ups, ys_ups, xs_step, ys_step, dest_w, cbuf, abuf);
+            }
+            else {
+                rgb_no_aa(src_buf, src_w, src_h, src_stride, xs_ups, ys_ups, xs_step, ys_step, dest_w, cbuf, abuf);
             }
         }
         else {
-            for(x = 0; x < dest_w; x++) {
-                int32_t xs_int = xs_ups >> 8;
-                int32_t ys_int = ys_ups >> 8;
-
-                /*Fully out of the image*/
-                if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
-                    abuf[x] = 0x00;
-                    ys_ups += ys_step;
-                    xs_ups += xs_step;
-                    continue;
-                }
-
-                /*Get the direction the hor and ver neighbor
-                 *`fract` will be in range of 0x00..0xFF and `next` (+/-1) indicates the direction*/
-                int32_t xs_fract = xs_ups & 0xFF;
-                int32_t ys_fract = ys_ups & 0xFF;
-                int32_t x_next;
-                int32_t y_next;
-                if(xs_fract < 0x80) {
-                    x_next = -1;
-                    xs_fract = (0x7F - xs_fract) * 2;
-                }
-                else {
-                    x_next = 1;
-                    xs_fract = (xs_fract - 0x80) * 2;
-                }
-                if(ys_fract < 0x80) {
-                    y_next = -1;
-                    ys_fract = (0x7F - ys_fract) * 2;
-                }
-                else {
-                    y_next = 1;
-                    ys_fract = (ys_fract - 0x80) * 2;
-                }
-
-                const uint8_t * src_tmp;
-                src_tmp = trans_dsc.cfg.src;
-                src_tmp += (ys_int * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE) + xs_int * LV_IMG_PX_SIZE_ALPHA_BYTE;
-
-                if(xs_int + x_next >= 0 &&
-                   xs_int + x_next <= src_w - 1 &&
-                   ys_int + y_next >= 0 &&
-                   ys_int + y_next <= src_h - 1) {
-
-                    const uint8_t * px_base = NULL;
-                    const uint8_t * px_hor = NULL;
-                    const uint8_t * px_ver = NULL;
-                    px_base = src_tmp;
-                    px_hor = src_tmp + x_next * LV_IMG_PX_SIZE_ALPHA_BYTE;
-                    px_ver = src_tmp + y_next * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE;
-
-                    lv_opa_t a_base = px_base[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
-                    lv_opa_t a_ver = px_ver[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
-                    lv_opa_t a_hor = px_hor[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
-
-                    if(a_ver != a_base) a_ver = ((a_ver * ys_fract) + (a_base * (0x100 - ys_fract))) >> 8;
-                    if(a_hor != a_base) a_hor = ((a_hor * xs_fract) + (a_base * (0x100 - xs_fract))) >> 8;
-                    abuf[x] = (a_ver + a_hor) >> 1;
-
-                    if(abuf[x]) {
-                        lv_color_t c_base;
-                        lv_color_t c_ver;
-                        lv_color_t c_hor;
-                        c_base.full = px_base[0] + (px_base[1] << 8);
-                        c_ver.full = px_ver[0] + (px_ver[1] << 8);
-                        c_hor.full = px_hor[0] + (px_hor[1] << 8);
-                        c_ver = lv_color_mix(c_ver, c_base, ys_fract);
-                        c_hor = lv_color_mix(c_hor, c_base, xs_fract);
-                        cbuf[x] = lv_color_mix(c_hor, c_ver, LV_OPA_50);
-                    }
-                }
-                else {
-                    cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
-                    lv_opa_t a = src_tmp[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
-
-                    if((xs_int == 0 && x_next < 0) || (xs_int == src_w - 1 && x_next > 0))  {
-                        abuf[x] = (a * (0xFF - xs_fract)) >> 8;
-                    }
-                    else if((ys_int == 0 && y_next < 0) || (ys_int == src_h - 1 && y_next > 0))  {
-                        abuf[x] = (a * (0xFF - ys_fract)) >> 8;
-                    }
-                    else {
-                        abuf[x] = 0x00;
-                    }
-                }
-
-                xs_ups += xs_step;
-                ys_ups += ys_step;
-            }
+            argb_and_rgb_aa(src_buf, src_w, src_h, src_stride, xs_ups, ys_ups, xs_step, ys_step, dest_w, cbuf, abuf, has_alpha);
         }
         cbuf += dest_w;
         abuf += dest_w;
@@ -220,6 +128,201 @@ void lv_draw_sw_transform(lv_draw_ctx_t * draw_ctx, const lv_area_t * dest_area,
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+
+static void rgb_no_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                      int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                      int32_t x_end, lv_color_t * cbuf, uint8_t * abuf)
+{
+    lv_memset_ff(abuf, x_end);
+
+    lv_coord_t x;
+    for(x = 0; x < x_end; x++) {
+        int32_t xs_int = xs_ups >> 8;
+        int32_t ys_int = ys_ups >> 8;
+        if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
+            abuf[x] = 0;
+        }
+        else {
+            const uint8_t * src_tmp = src;
+            src_tmp += (ys_int * src_stride * sizeof(lv_color_t)) + xs_int * sizeof(lv_color_t);
+
+#if LV_COLOR_DEPTH == 8
+            cbuf[x].full = src_tmp[0];
+#elif LV_COLOR_DEPTH == 16
+            cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+            cbuf[x].full = *((uint32_t *)src_tmp);
+#endif
+        }
+        xs_ups += xs_step;
+        ys_ups += ys_step;
+    }
+}
+
+static void argb_no_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                       int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                       int32_t x_end, lv_color_t * cbuf, uint8_t * abuf)
+{
+    lv_coord_t x;
+    for(x = 0; x < x_end; x++) {
+        int32_t xs_int = xs_ups >> 8;
+        int32_t ys_int = ys_ups >> 8;
+        if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
+            abuf[x] = 0;
+        }
+        else {
+            const uint8_t * src_tmp = src;
+            src_tmp += (ys_int * src_stride * LV_IMG_PX_SIZE_ALPHA_BYTE) + xs_int * LV_IMG_PX_SIZE_ALPHA_BYTE;
+
+#if LV_COLOR_DEPTH == 8
+            cbuf[x].full = src_tmp[0];
+#elif LV_COLOR_DEPTH == 16
+            cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+            cbuf[x].full = *((uint32_t *)src_tmp);
+#endif
+            abuf[x] = src_tmp[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+        }
+        xs_ups += xs_step;
+        ys_ups += ys_step;
+    }
+}
+
+static void argb_and_rgb_aa(const uint8_t * src, lv_coord_t src_w, lv_coord_t src_h, lv_coord_t src_stride,
+                            int32_t xs_ups, int32_t ys_ups, int32_t xs_step, int32_t ys_step,
+                            int32_t x_end, lv_color_t * cbuf, uint8_t * abuf, bool has_alpha)
+{
+
+    int32_t px_size;
+    if(!has_alpha) {
+        lv_memset_ff(abuf, x_end);
+        px_size = sizeof(lv_color_t);
+    }
+    else {
+        px_size = LV_IMG_PX_SIZE_ALPHA_BYTE;
+    }
+
+    lv_coord_t x;
+    for(x = 0; x < x_end; x++) {
+        int32_t xs_int = xs_ups >> 8;
+        int32_t ys_int = ys_ups >> 8;
+
+        /*Fully out of the image*/
+        if(xs_int < 0 || xs_int >= src_w || ys_int < 0 || ys_int >= src_h) {
+            abuf[x] = 0x00;
+            ys_ups += ys_step;
+            xs_ups += xs_step;
+            continue;
+        }
+
+        /*Get the direction the hor and ver neighbor
+         *`fract` will be in range of 0x00..0xFF and `next` (+/-1) indicates the direction*/
+        int32_t xs_fract = xs_ups & 0xFF;
+        int32_t ys_fract = ys_ups & 0xFF;
+        int32_t x_next;
+        int32_t y_next;
+        if(xs_fract < 0x80) {
+            x_next = -1;
+            xs_fract = (0x7F - xs_fract) * 2;
+        }
+        else {
+            x_next = 1;
+            xs_fract = (xs_fract - 0x80) * 2;
+        }
+        if(ys_fract < 0x80) {
+            y_next = -1;
+            ys_fract = (0x7F - ys_fract) * 2;
+        }
+        else {
+            y_next = 1;
+            ys_fract = (ys_fract - 0x80) * 2;
+        }
+
+        const uint8_t * src_tmp = src;
+        src_tmp += (ys_int * src_stride * px_size) + xs_int * px_size;
+
+        if(xs_int + x_next >= 0 &&
+           xs_int + x_next <= src_w - 1 &&
+           ys_int + y_next >= 0 &&
+           ys_int + y_next <= src_h - 1) {
+
+            const uint8_t * px_base = NULL;
+            const uint8_t * px_hor = NULL;
+            const uint8_t * px_ver = NULL;
+            px_base = src_tmp;
+            px_hor = src_tmp + x_next * px_size;
+            px_ver = src_tmp + y_next * src_stride * px_size;
+
+            if(has_alpha) {
+                lv_opa_t a_base = px_base[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+                lv_opa_t a_ver = px_ver[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+                lv_opa_t a_hor = px_hor[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+
+                if(a_ver != a_base) a_ver = ((a_ver * ys_fract) + (a_base * (0x100 - ys_fract))) >> 8;
+                if(a_hor != a_base) a_hor = ((a_hor * xs_fract) + (a_base * (0x100 - xs_fract))) >> 8;
+                abuf[x] = (a_ver + a_hor) >> 1;
+            }
+
+            if(abuf[x]) {
+                lv_color_t c_base;
+#if LV_COLOR_DEPTH == 8
+                c_base.full = px_base[0];
+#elif LV_COLOR_DEPTH == 16
+                c_base.full = px_base[0] + (px_base[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+                c_base.full = *((uint32_t *)px_base);
+#endif
+
+                lv_color_t c_ver;
+#if LV_COLOR_DEPTH == 8
+                c_ver.full = px_ver[0];
+#elif LV_COLOR_DEPTH == 16
+                c_ver.full = px_ver[0] + (px_ver[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+                c_ver.full = *((uint32_t *)px_ver);
+#endif
+
+                lv_color_t c_hor;
+#if LV_COLOR_DEPTH == 8
+                c_hor.full = px_hor[0];
+#elif LV_COLOR_DEPTH == 16
+                c_hor.full = px_hor[0] + (px_hor[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+                c_hor.full = *((uint32_t *)px_hor);
+#endif
+
+                c_ver = lv_color_mix(c_ver, c_base, ys_fract);
+                c_hor = lv_color_mix(c_hor, c_base, xs_fract);
+                cbuf[x] = lv_color_mix(c_hor, c_ver, LV_OPA_50);
+            }
+        }
+        else {
+
+#if LV_COLOR_DEPTH == 8
+            cbuf[x].full = src_tmp[0];
+#elif LV_COLOR_DEPTH == 16
+            cbuf[x].full = src_tmp[0] + (src_tmp[1] << 8);
+#elif LV_COLOR_DEPTH == 32
+            cbuf[x].full = *((uint32_t *)src_tmp);
+#endif
+
+            lv_opa_t a = has_alpha ? 0xff : src_tmp[LV_IMG_PX_SIZE_ALPHA_BYTE - 1];
+
+            if((xs_int == 0 && x_next < 0) || (xs_int == src_w - 1 && x_next > 0))  {
+                abuf[x] = (a * (0xFF - xs_fract)) >> 8;
+            }
+            else if((ys_int == 0 && y_next < 0) || (ys_int == src_h - 1 && y_next > 0))  {
+                abuf[x] = (a * (0xFF - ys_fract)) >> 8;
+            }
+            else {
+                abuf[x] = 0x00;
+            }
+        }
+
+        xs_ups += xs_step;
+        ys_ups += ys_step;
+    }
+}
 
 /**
  * Initialize a descriptor to transform an image
