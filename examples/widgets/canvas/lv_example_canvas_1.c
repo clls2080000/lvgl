065a4fc3db7fde2d07d0fdd9e176168375e12bab
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-04-20 23:17:43 +0200

    make canvas rotation work again

--- examples/widgets/canvas/lv_example_canvas_1.c
+++ examples/widgets/canvas/lv_example_canvas_1.c
@@ -37,7 +37,7 @@ void lv_example_canvas_1(void)
     lv_canvas_draw_text(canvas, 40, 20, 100, &label_dsc, "Some text on text canvas");
 
     /*Test the rotation. It requires another buffer where the original image is stored.
-     *So copy the current image to buffer and rotate it to the canvas*/
+     *So copy the current image to a buffer and rotate it to the canvas*/
     static lv_color_t cbuf_tmp[CANVAS_WIDTH * CANVAS_HEIGHT];
     memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
     lv_img_dsc_t img;
